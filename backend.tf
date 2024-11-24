db710b2a70cbc71aafb2b2b85bcac52b6964a971
04Anshul <anshulgautam04@gmail.com>
2024-11-25 02:26:43 +0530

    new q

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "github-actions-demo-seoul"
-    key    = "github-actions-seoul.tfstate"
+    key    = "github-actions-demo-seoul.tfstate"
     region = "ap-northeast-2"
     dynamodb_table = "products"
     encrypt        = true
